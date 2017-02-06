343cbd6e7f9e9d03623b595b6be6356a953575e1
Lusito <core@roughael.net>
2017-02-06 18:53:19 +0100

    context menu as panel

--- scripts/contextmenu.js
+++ scripts/contextmenu.js
@@ -83,6 +83,7 @@
                 translator.run({
                     text: wordUnderCursor,
                     languagePair: translation.k,
+                    asPanel: settings.get('context.asPanel'),
                     x: lastCursorX,
                     y: lastCursorY
                 }, false, tab);
