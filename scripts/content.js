8cd2d3f7549fcf29d97242276199902c716f9b09
Lusito <core@roughael.net>
2017-06-29 23:41:56 +0200

    simplifying license block and creating icon folder

--- scripts/content.js
+++ scripts/content.js
@@ -4,18 +4,18 @@
  
  * This file is part of the dict.cc web-extension.
  
- * The dict.cc web-extension is free software: you can redistribute it and/or modify
+ * This web-extension is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  
- * The dict.cc web-extension is distributed in the hope that it will be useful,
+ * This web-extension is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  
  * You should have received a copy of the GNU General Public License
- * along with the dict.cc web-extension.  If not, see http://www.gnu.org/licenses/.
+ * along with this web-extension.  If not, see http://www.gnu.org/licenses/.
  
  * ***** END LICENSE BLOCK ***** */
 
@@ -299,7 +299,7 @@ function MiniLayer(x, y, onload) {
 
         var div = createElement(idoc, ibody, 'div');
         var a = createElement(idoc, div, 'a', {target: "_blank", href: "http://www.dict.cc/", id: "logo"});
-        createElement(idoc, a, 'img', {src: browser.runtime.getURL("icon16.png"), alt: "dict.cc"});
+        createElement(idoc, a, 'img', {src: browser.runtime.getURL("icons/icon16.png"), alt: "dict.cc"});
         resultNode = createElement(idoc, div, 'span', {id: "result"});
         extraNode = createElement(idoc, ibody, 'span', {id: "extra"});
         setTimeout(onload, 0);
